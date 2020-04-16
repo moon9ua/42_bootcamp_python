278866cc228797b7dbe6f1320b342937c82afc73
moon9ua <koom6@naver.com>
2020-04-17 01:52:21 +0900

    20.04.16

--- day00/ex01/exec.py
+++ day00/ex01/exec.py
@@ -1,2 +1,21 @@
-a = input()
-print(a)
+import sys
+
+text = ""
+
+for i in range(1, len(sys.argv)):
+	if text == "":
+		text = sys.argv[i]
+	else:
+		text = text + " " + sys.argv[i]
+	i = i + 1
+
+new_text = ""
+
+for i in range(len(text) - 1, -1, -1):
+	if text[i].isupper() == True:
+		new_text = new_text + text[i].lower()
+	else:
+		new_text = new_text + text[i].upper()
+	i = i - 1
+
+print(new_text)
