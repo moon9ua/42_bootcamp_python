b5ac57a65f35b2c0f79664611fbe4e05579d2c21
moon9ua <koom6@naver.com>
2020-04-18 17:18:45 +0900

    20.04.17

---
+++ day00/ex02/whois.py
@@ -0,0 +1,12 @@
+import sys
+
+if (len(sys.argv) == 1):
+	()
+elif (sys.argv[1].isdigit() == False or len(sys.argv) != 2):
+	print("ERROR")
+elif (int(sys.argv[1]) == 0):
+	print("I'm Zero.")
+elif (int(sys.argv[1]) % 2 == 1):
+	print("I'm Odd.")
+elif (int(sys.argv[1]) % 2 == 0):
+	print("I'm Even.")
